f2b77233cacdca6e73e12a6bd5dc8964fdd6e28a
Jaylon Delgado <jaylondelgado1124@gmail.com>
2022-03-28 16:15:23 -0700

    ft 5 almost done

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import ListView
+from projects.models import Project
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+
+
+class ProjectListView(LoginRequiredMixin, ListView):
+    model = Project
+    template_name = "projects/projects_list.html"
