74c69cec67a851131b0dee9bb73cf9e5d363f234
Jaylon Delgado <jaylondelgado1124@gmail.com>
2022-03-28 15:27:33 -0700

    ft 4 done

--- projects/models.py
+++ projects/models.py
@@ -6,6 +6,8 @@ from django.shortcuts import redirect
 USER_MODEL = settings.AUTH_USER_MODEL
 
 # Create your models here.
+
+
 class Project(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=False)
