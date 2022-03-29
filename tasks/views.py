dbf4a0a7c4e40a906014b3224b6e2180b482245d
Jaylon Delgado <jaylondelgado1124@gmail.com>
2022-03-29 15:59:39 -0700

    17 done

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import redirect
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from tasks.models import Task
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -18,3 +19,11 @@ class TaskListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Task.objects.filter(assignee=self.request.user)
+
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = Task
+    template_name = "tasks/tasks_update.html"
+    fields = ["is_completed"]
+
+    success_url = reverse_lazy("show_my_tasks")
