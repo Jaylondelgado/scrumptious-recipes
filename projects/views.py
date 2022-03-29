e8255aabb3fc6223a90420da31af825a80c5c07b
Jaylon Delgado <jaylondelgado1124@gmail.com>
2022-03-28 18:24:31 -0700

    ft 7

--- projects/views.py
+++ projects/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 
-class ProjectListView(ListView):
+class ProjectListView(LoginRequiredMixin, ListView):
     model = Project
     template_name = "projects/projects_list.html"
+
+    def get_queryset(self):
+        return Project.objects.filter(members=self.request.user)
