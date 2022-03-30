42c34e90d162398b424877c69307885ffe07dee6
Jaylon Delgado <jaylondelgado1124@gmail.com>
2022-03-30 11:26:25 -0700

    all tests passed

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,6 @@
 from django.views.generic import ListView, DetailView, CreateView
 from projects.models import Project
 from django.contrib.auth.mixins import LoginRequiredMixin
-from tasks.models import Task
 
 
 # Create your views here.
@@ -14,21 +13,11 @@ class ProjectListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         return Project.objects.filter(members=self.request.user)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["tasks"] = Task.objects.all()
-        return context
-
 
 class ProjectDetailView(LoginRequiredMixin, DetailView):
     model = Project
     template_name = "projects/projects_detail.html"
     context_object_name = "project_detail"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["tasks"] = Task.objects.all()
-        return context
 
 
 class ProjectCreateView(LoginRequiredMixin, CreateView):
